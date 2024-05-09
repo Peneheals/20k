72b155de7bcb9af8370dcbf96702232c3cfe3214
gazdagergo <info@gazdagergo.com>
2024-05-09 18:41:00 +0200

    upgrade nextjs

--- components/Menu/index.jsx
+++ components/Menu/index.jsx
@@ -117,9 +117,7 @@ const Menu = ({ isBig = true }) => {
       >
       <Inner>
         <Link href="/">
-          <a>
-            <TopLogo />
-          </a>
+          <TopLogo />
         </Link>
         <MenuIcon onClick={() => setMobileNavOpen(!isMobileNavOpen)} />
         <MenuWrap>
